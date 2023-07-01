8b42e9b6870521b391700cadda35f7964234bd19
Elsayed Hamada <elsayed.7work@gmail.com>
2023-07-01 15:42:02 +0300

    Mac Section

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ const closeNav = () => {
 
 close.addEventListener("click", closeNav);
 darkBg.addEventListener("click", closeNav);
+
+// Laptop & Desktop Switch
+const desktopContainer = document.querySelector(".desktop-container");
+const laptopsContainer = document.querySelector(".laptops-container");
+const switchContainer = document.querySelector(".switch");
+
+switchContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("desktop")) {
+    desktopContainer.classList.remove("inactive");
+    laptopsContainer.classList.add("inactive");
+    e.target.style.borderBottom = "solid 2px #313131";
+    e.target.previousElementSibling.style.borderBottom = "none";
+  } else {
+    desktopContainer.classList.add("inactive");
+    laptopsContainer.classList.remove("inactive");
+    e.target.style.borderBottom = "solid 2px #313131";
+    e.target.nextElementSibling.style.borderBottom = "none";
+  }
+});
