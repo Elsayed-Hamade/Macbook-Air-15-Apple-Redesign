d4bd8e5e40a472793c1abe8d418d3ec23a2d403f
Elsayed Hamada <elsayed.7work@gmail.com>
2023-07-02 00:37:55 +0300

    slider

--- script.js
+++ script.js
@@ -17,20 +17,66 @@ close.addEventListener("click", closeNav);
 darkBg.addEventListener("click", closeNav);
 
 // Laptop & Desktop Switch
-const desktopContainer = document.querySelector(".desktop-container");
+const productsContainer = document.querySelectorAll(".products-container");
 const laptopsContainer = document.querySelector(".laptops-container");
+const desktopContainer = document.querySelector(".desktop-container");
 const switchContainer = document.querySelector(".switch");
+const switchChildren = Array.from(switchContainer.children);
 
 switchContainer.addEventListener("click", (e) => {
+  const clicked = e.target.closest(".switch-btn");
+  if (!clicked) return;
+
+  switchChildren.forEach((child) => {
+    child.style.borderBottom = "none";
+  });
+
+  productsContainer.forEach((product) => {
+    product.classList.add("inactive");
+  });
+
+  const addBorder = (target) => {
+    target.style.borderBottom = "solid 2px #313131";
+  };
+
   if (e.target.classList.contains("desktop")) {
     desktopContainer.classList.remove("inactive");
-    laptopsContainer.classList.add("inactive");
-    e.target.style.borderBottom = "solid 2px #313131";
-    e.target.previousElementSibling.style.borderBottom = "none";
+    addBorder(e.target);
   } else {
-    desktopContainer.classList.add("inactive");
     laptopsContainer.classList.remove("inactive");
-    e.target.style.borderBottom = "solid 2px #313131";
-    e.target.nextElementSibling.style.borderBottom = "none";
+    addBorder(e.target);
   }
 });
+
+// Iphone Slider
+const slides = document.querySelectorAll(".iphone-product");
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+
+let count = 0;
+const maxCount = slides.length;
+const gotoSlide = (currCount) => {
+  slides.forEach((slide, i) => {
+    slide.style.transform = `translateX(${100 * (i - currCount)}%)`;
+  });
+};
+
+rightArrow.addEventListener("click", () => {
+  if (count === maxCount - 1) {
+    count = 0;
+  } else {
+    count++;
+  }
+  gotoSlide(count);
+});
+
+leftArrow.addEventListener("click", () => {
+  if (count === 0) {
+    count = maxCount - 1;
+  } else {
+    count--;
+  }
+  gotoSlide(count);
+});
+
+gotoSlide(0);
